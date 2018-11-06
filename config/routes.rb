a7bb7af63947492d4d14bfeb1f80a06bd0700174
Nicole Velasco <nicolevelasco455@gmail.com>
2018-11-06 16:35:53 -0300

    task app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Read all tasks
+  get 'tasks', to: 'tasks#index', as: :tasks
+
+  # Create one task (2 requests)
+  # 1. get empty form
+  # 2. Request to post the params
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # Read one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # Update one task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # Delete one task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: 'Laundry', details: 'Do not mix colors!')
+Task.create(title: 'Studying', details: 'A lot of flashcards to do')
+Task.create(title: 'Laundry', details: 'wash pjs and jeans', completed: true)
+Task.create(title: 'Wash dishes', details: 'everything!!')
+Task.create(title: 'Do flashcards', details: 'ever since javascript')
+Task.create(title: 'Update Linkdin', details: 'update everything')
